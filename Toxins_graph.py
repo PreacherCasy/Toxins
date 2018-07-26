d831099854f135ce32c2da5d54fda310fb6830e7
PreacherCasy <271296251017a@gmail.com>
2018-07-26 04:23:23 +0300

    Update Toxins_graph.py
    revised connected component implementation plus argparse realization

--- Toxins_graph.py
+++ Toxins_graph.py
@@ -3,15 +3,15 @@ from Bio import SeqIO
 from Bio.Seq import Seq
 from collections import defaultdict
 from graphviz import Digraph
-import pydot
+import argparse
+
 
 
 class Vertex():
     def __init__(self, name, seq):
         self.name = name
         self.seq = seq
-        self.adj = defaultdict()
-
+        self.adj = dict()
 
 class Edge():
     def __init__(self, v1, v2):
@@ -19,57 +19,46 @@ class Edge():
         self.v2 = v2
         self.subst = list()
 
-
-class Graph():
+class graph():
     def __init__(self):
-        self.vertices = defaultdict()
+        self.vertices = dict()
+        self.edges = dict()
 
     def add_vertex(self, file):
         for record in SeqIO.parse(file, 'fasta'):
             self.vertices[(Vertex(record.id, str(record.seq)))] = []
 
-    def adjacency_list(self):
+    def adjacency_list(self, k):
         for vertex in self.vertices.keys():
-            for vert in self.vertices.keys():
+            for vert in list(self.vertices.keys())[list(self.vertices.keys()).index(vertex):]:
                 if vert.name != vertex.name:
                     local_edge = Edge(vertex, vert)
                     for i in range(0, len(vertex.seq)):
                         if vertex.seq[i] != vert.seq[i]:
                             local_edge.subst.append(f"{vertex.seq[i]}{i+1}{vert.seq[i]}")
-                    self.vertices[vertex].append({vert:local_edge})
+                    if len(local_edge.subst) <= k:
+                        self.vertices[vertex].append([vert, local_edge])
 
-    # def visualize(self):
-    #     vis = Digraph(comment='Cry toxin amino acid substitution graph')
-    #     for i, j in self.vertices.items():
-    #         vis.node(i.name, label=f"{i.name}")
-    #         for num in j:
-    #             for el in num.keys():
-    #                 print(num[el].subst, '\n', i.name, el.name)
-    #                 vis.edge(i.name, el.name, label=", ".join(num[el].subst))
-    #     vis.view()
-    #     vis.save()
-
-    def visualize(self):
+    def component(self):
         vis = Digraph(comment='Cry toxin amino acid substitution graph')
         for i, j in self.vertices.items():
             vis.node(i.name, label=f"{i.name}")
-            temp, eq = [], 1000
-            for num in j:
-                for el in num.items():
-                    if len(el[1].subst) < eq:
-                        temp = [el]
-                    elif len(el[1].subst) == eq:
-                        temp.append(el)
-                    eq = len(el[1].subst)
-                for item in temp:
-                    vis.edge(i.name, item[0].name, label=", ".join(item[1].subst))
+            for item in j:
+               vis.edge(i.name, item[0].name, label = ", ".join(item[1].subst))
+
         vis.view()
         vis.save()
 
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Cry toxin amino acid substitution graph')
+    parser.add_argument('-k', help='Maximal number of mismatches for adjacency', metavar='Int', type=int, default=1)
+        parser.add_argument('-p', help='add path to a file', metavar='Str', type=str)
+    args = parser.parse_args()
+    k = args.k
+    p = args.p
 
-my_graph = Graph()
-my_graph.add_vertex('/home/reverend_casy/toxin_dummy_aligned.fasta')
-my_graph.adjacency_list()
-
-my_graph.visualize()
+    my_graph = graph()
+    my_graph.add_vertex(f"{p}")
+    my_graph.adjacency_list(k)
+    my_graph.component()
