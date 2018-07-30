eacfcbd18cb6848f1cb1a201736db92c51c9f302
PreacherCasy <271296251017a@gmail.com>
2018-07-31 00:13:41 +0300

    Update Toxins_graph.py

--- Toxins_graph.py
+++ Toxins_graph.py
@@ -1,17 +1,15 @@
-import Bio
 from Bio import SeqIO
-from Bio.Seq import Seq
 from collections import defaultdict
 from graphviz import Digraph
 import argparse
-
-
 
 class Vertex():
     def __init__(self, name, seq):
         self.name = name
         self.seq = seq
-        self.adj = dict()
+        self.in_edges = dict()
+        self.out_edges = dict()
+        self.baseline = 0
 
 class Edge():
     def __init__(self, v1, v2):
@@ -21,38 +19,45 @@ class Edge():
 
 class graph():
     def __init__(self):
-        self.vertices = dict()
-        self.edges = dict()
+        self.vertices = list()
+        self.check = dict()
 
     def add_vertex(self, file):
         for record in SeqIO.parse(file, 'fasta'):
-            self.vertices[(Vertex(record.id, str(record.seq)))] = []
+            self.vertices.append(Vertex(record.id, str(record.seq)))
+
+    def acyclic(self, node1, node2):
+        print("beep")
+        if node1 in node2.out_edges.keys():
+            return False
+        else:
+            for i in node2.out_edges.keys():
+                self.acyclic(node1, i)
 
     def adjacency_list(self):
-        for vertex in self.vertices.keys():
-
-            for vert in list(self.vertices.keys())[list(self.vertices.keys()).index(vertex):]:
-                print(vert.name)
-                if vert.name != vertex.name:
+        for vertex in self.vertices:
+            for vert in self.vertices[self.vertices.index(vertex):]:
+                if vert.name != vertex.name  and self.acyclic(vert, vertex) != False:
                     local_edge = Edge(vertex, vert)
                     for i in range(0, len(vertex.seq)):
                         if vertex.seq[i] != vert.seq[i]:
                             local_edge.subst.append(f"{vertex.seq[i]}{i+1}{vert.seq[i]}")
-#                    if len(local_edge.subst) <= k:
-                    try:
-                        if len(local_edge.subst) < len(self.vertices[vertex][0][1].subst):
-                            self.vertices[vertex][0] = [vert, local_edge]
-                        elif len(local_edge.subst) == len(self.vertices[vertex][0][1].subst):
-                            self.vertices[vertex].append([vert, local_edge])
-                    except:
-                        self.vertices[vertex].append( [vert, local_edge])
+                    if len(local_edge.subst) < vertex.baseline:
+                        vertex.out_edges.clear()
+                        vertex.out_edges[vert] = local_edge
+                        vertex.baseline = len(local_edge.subst)
+                        vert.in_edges.clear()
+                        vert.in_edges[vertex] = local_edge
+                    elif len(local_edge.subst) == vertex.baseline or vertex.baseline == 0:
+                        vertex.out_edges[vert] = local_edge
+                        vertex.baseline = len(local_edge.subst)
+                        vert.in_edges[vertex] = local_edge
 
-
-    def component(self):
+    def visualize(self):
         vis = Digraph(comment='Cry toxin amino acid substitution graph')
-        for i, j in self.vertices.items():
+        for i in self.vertices:
             vis.node(i.name, label=f"{i.name}")
-            for item in j:
+            for item in i.out_edges.items():
                 vis.edge(i.name, item[0].name, label = ", ".join(item[1].subst))
 
         vis.view()
