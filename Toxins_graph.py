0bd8acbe26c698150376c61d52e337ec840e7159
PreacherCasy <271296251017a@gmail.com>
2018-10-04 01:21:17 +0300

    Adjacency matrix-based version of graph
    NOTE: EMBOSS launcher and raw distmat parser are not implemented yet

--- Toxins_graph.py
+++ Toxins_graph.py
@@ -1,81 +1,85 @@
 from Bio import SeqIO
-from graphviz import Digraph
+import numpy as np
+from scipy.sparse.csgraph import connected_components as cc
+from graphviz import Graph
 import argparse
-
-class Vertex():
-    def __init__(self, name, seq):
-        self.name = name
-        self.seq = seq
-        self.out_edges = dict()
-        self.baseline = 0
-
-class Edge():
-    def __init__(self, v1, v2):
-        self.v1 = v1
-        self.v2 = v2
-        self.subst = list()
+import subprocess
 
 class graph():
-    def __init__(self):
-        self.vertices = list()
-        self.adj_list = dict()
-
-    def add_vertex(self, file):
-        for record in SeqIO.parse(file, 'fasta'):
-            self.vertices.append(Vertex(record.id, str(record.seq)))
-
-
-    def find_path(self, start, end, path):
-        try:
-            path = path + [start]
-            if start == end:
-                return True
-            for node in self.adj_list[start]:
-                if node not in path:
-                    newpath = self.find_path(node, end, path)
-                    if newpath: return True
-            return False
-        except:
-            return False
-
-    def checker(self, start, end, path):
-        if self.find_path(start, end, path) == True:
-            return False
-        else:
-            return True
+    def __init__(self, distances, alignment):
+        self.distances = distances
+        self.alignment = alignment
+        self.adj_matrix = np.triu(np.full((11,11), 1), 1)
+        self.vertices = dict()
+        self.edges = list()
+        self.distmat = np.zeros((11, 11))
 
 
+    def populate_distances(self):
+        filling_list = []
+        with open(self.distances) as handle:
+            for line in handle:
+                line = list(map(float, filter(None, line.replace('\t', ' ').replace('\n', ' ').split(' '))))
+                filling_list.append(line)
+        for i in range(0, len(filling_list)):
+            for j in range(0, len(filling_list[i])):
+                self.distmat[i, -j] = filling_list[i][-j]
 
-    def adjacency_list(self):
-        for vertex in self.vertices:
-            self.adj_list[vertex] = list()
-            for vert in self.vertices:
-                if vert.name != vertex.name and self.checker(vert, vertex, path = []) is True and vertex not in vert.out_edges.keys():
-                    local_edge = Edge(vertex, vert)
-                    for i in range(0, len(vertex.seq)):
-                        if vertex.seq[i] != vert.seq[i]:
-                            local_edge.subst.append(f"{vertex.seq[i]}{i+1}{vert.seq[i]}")
-                    if len(local_edge.subst) < vertex.baseline:
-                        vertex.out_edges.clear()
-                        vertex.out_edges[vert], vertex.baseline = local_edge, len(local_edge.subst)
-                        self.adj_list[vertex].clear()
-                        self.adj_list[vertex].append(vert)
-                    elif len(local_edge.subst) == vertex.baseline or vertex.baseline == 0:
-                        vertex.out_edges[vert], vertex.baseline = local_edge, len(local_edge.subst)
-                        self.adj_list[vertex].append(vert)
-        print(self.adj_list)
+    def edge_cutter(self):
+        if np.count_nonzero(self.distmat) > 0:
+            i, j = np.argwhere(self.distmat == np.amax(self.distmat))[0][0], np.argwhere(self.distmat
+                                                                                             == np.amax(self.distmat))[0][1]
+            self.adj_matrix[i][j] = 0
+            if cc(self.adj_matrix)[0] != 1:
+                self.adj_matrix[i][j] = 1
+            self.distmat[i][j] = 0
+            self.edge_cutter()
+
+    def upload_vertices(self):
+        vert_list, self.g = list(), dict()
+        for record in SeqIO.parse(self.alignment, 'fasta'):
+            vert_list.append(record)
+
+        for i in range(0, len(vert_list)):
+            self.g[vert_list[i].id] = []
+            for j in range(0, len(self.adj_matrix[i, :])):
+                label = list()
+                if self.adj_matrix[i, j] == 1:
+                    for pos in range(0, len(vert_list[j].seq)):
+                        if vert_list[i].seq[pos] != vert_list[j].seq[pos]:
+                            label.append(f"{vert_list[i].seq[pos]}{pos + 1}{vert_list[j].seq[pos]}")
+                    self.g[vert_list[i].id].append((vert_list[j].id, label))
 
     def visualize(self):
-        vis = Digraph(comment='Cry toxin amino acid substitution graph', strict=True)
-        for i in self.vertices:
-            vis.node(i.name, label=f"{i.name}")
-            for item in i.out_edges.items():
-                vis.edge(i.name, item[0].name, label = ", ".join(item[1].subst))
+        self.upload_vertices()
+        vis = Graph(comment='Cry toxin amino acid substitution graph', strict=True)
+        for i in self.g.keys():
+            vis.node(i, label=f"{i}")
+            for item in self.g[i]:
+                vis.edge(i, item[0], label=", ".join(item[1]))
 
         vis.view()
         vis.save()
 
-my_graph = graph()
-my_graph.add_vertex('/home/reverend_casy/toy_set.fasta')
-my_graph.adjacency_list()
-my_graph.visualize()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='graph_viualization')
+    parser.add_argument('-ai', help='Name/path to your fasta file', metavar='File',
+                        type=str, required=True)
+    parser.add_argument('-ao', help='Name/path to your alignment file', metavar='File',
+                        type=str, required=True)
+    parser.add_argument('-d', help='Name/path to your distance matrix file', metavar='File',
+                        type=str, required=True)
+    # parser.add_argument('-ms', help='MAFFT settings',
+    #                     type=str, required=False, default=None)
+    parser.set_defaults(feature=True)
+
+    args = parser.parse_args()
+    d, ai, ao = args.d, args.ai, args.ao
+
+    subprocess.call(f"mafft {ai} > {ao}", shell=True)
+    g = graph(d, ao)
+    g.populate_distances()
+    g.edge_cutter()
+    g.visualize()
