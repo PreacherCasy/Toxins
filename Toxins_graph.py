8eaa83f5048ae5f7a3d9ab2d93cd76899a0aa0f1
PreacherCasy <271296251017a@gmail.com>
2019-02-15 23:14:05 +0300

    Update Toxins_graph.py

--- Toxins_graph.py
+++ Toxins_graph.py
@@ -1,70 +1,61 @@
 from Bio import SeqIO
-from Bio.Phylo.TreeConstruction import DistanceCalculator
 from Bio import AlignIO
 import numpy as np
-from scipy.sparse.csgraph import connected_components as cc
+from collections import defaultdict
 from graphviz import Graph
 import argparse
 import subprocess
 
-class graph():
+
+class AAGraph():
     def __init__(self, alignment):
         self.alignment = alignment
-        self.distmat = np.triu(DistanceCalculator('identity').get_distance(AlignIO.read(open(alignment), 'fasta')),1)
-        self.adj_matrix = np.triu(np.full((len(AlignIO.read(open(alignment), 'fasta')), len(AlignIO.read(open(alignment),
-                                                                                                'fasta'))), 1), 1)
-        self.vertices = dict()
-
-    def edge_cutter(self):
-        if np.count_nonzero(self.distmat) > 0:
-            i, j = np.argwhere(self.distmat == np.amax(self.distmat))[0][0], np.argwhere(self.distmat
-                                                                                             == np.amax(self.distmat))[0][1]
-            self.adj_matrix[i][j] = 0
-            if cc(self.adj_matrix)[0] != 1:
-                self.adj_matrix[i][j] = 1
-            self.distmat[i][j] = 0
-            self.edge_cutter()
+        self.al = AlignIO.read(open(self.alignment), 'fasta')
+        self.adjMat = np.triu(np.full((len(AlignIO.read(open(alignment), 'fasta')), len(AlignIO.read(open(alignment), 'fasta'))), 1))
+        np.fill_diagonal(self.adjMat, 0)
+        self.distMat = np.zeros((len(AlignIO.read(open(alignment), 'fasta')), len(AlignIO.read(open(alignment), 'fasta'))))
+        self.vertices = list()
+        self.edges = defaultdict(list)
 
     def upload_vertices(self):
-        vert_list, self.g = list(), dict()
-        for record in SeqIO.parse(self.alignment, 'fasta'):
-            vert_list.append(record)
+        for entry in self.al:
+            self.vertices.append(entry.id)
 
-        for i in range(0, len(vert_list)):
-            self.g[vert_list[i].id] = []
-            for j in range(0, len(self.adj_matrix[i, :])):
-                label = list()
-                if self.adj_matrix[i, j] == 1:
-                    for pos in range(0, len(vert_list[j].seq)):
-                        if vert_list[i].seq[pos] != vert_list[j].seq[pos]:
-                            label.append(f"{vert_list[i].seq[pos]}{pos + 1}{vert_list[j].seq[pos]}")
-                    self.g[vert_list[i].id].append((vert_list[j].id, label))
+    def matBuild(self):
+        self.distMat = np.zeros((len(self.al), len(self.al)))
+        for k in range(1, len(self.al)):
+            for i in range(k):
+                for j in range(0, len(self.al[i].seq)):
+                    if self.al[k].seq[j] != self.al[i].seq[j]:
+                        self.distMat[i][k] += 1
+                        self.edges[(self.al[k].id, self.al[i].id)].\
+                            append(f"{self.al[k].seq[j]}{str(j + 1)}{self.al[i].seq[j]}")
+        self.distMat = self.distMat
+
+    def TR(self):
+        for k in range(1, len(self.distMat)):
+            for i in range(k):
+                for j in range(k + 1, len(self.distMat)):
+                    viaK = max(self.distMat[i, k], self.distMat[k, j])
+                    if viaK < abs(self.distMat[i, j]) and abs(self.distMat[i, j])!= 0:
+                        self.distMat[i, j] = -viaK
+            for i in range(len(self.distMat)):
+                for j in range(len(self.distMat)):
+                    if self.distMat[i, j] < 0:
+                        self.adjMat[i, j] = 0
 
     def visualize(self):
-        self.upload_vertices()
         vis = Graph(comment='Cry toxin amino acid substitution graph', strict=True)
-        for i in self.g.keys():
+        for start, i in enumerate(self.vertices):
             vis.node(i, label=f"{i}")
-            for item in self.g[i]:
-                vis.edge(i, item[0], label=", ".join(item[1]))
-
+            for end in range(start, ):
+                if self.adjMat[end][start] == 1:
+                    item = (i, self.vertices[end])
+                    vis.edge(i, self.vertices[end], label=", ".join(self.edges[item]))
         vis.view()
-        vis.save()
 
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='graph_viualization')
-    parser.add_argument('-ai', help='Name/path to your fasta file', metavar='File',
-                        type=str, required=True)
-    parser.add_argument('-ao', help='Name/path to your alignment file', metavar='File',
-                        type=str, required=True)
-    parser.set_defaults(feature=True)
-
-    args = parser.parse_args()
-    ai, ao = args.ai, args.ao
-
-    subprocess.call(f"mafft {ai} > {ao}", shell=True)
-    g = graph(ao)
-    g.edge_cutter()
-    g.visualize()
+dum = AAGraph('/home/reverend_casy/Bt_toxins/toy_set_aligned.fasta')
+dum.upload_vertices()
+dum.matBuild()
+dum.TR()
+dum.visualize()
